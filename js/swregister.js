09dec53776f8e0e8324f7b6877269c5bcc7d08ff
rohith123 <rohithkumar19999@gmail.com>
2019-02-16 14:39:13 +0530

    hii

---
+++ js/swregister.js
@@ -0,0 +1,15 @@
+if('serviceWorker' in navigator){
+	navigator.serviceWorker.register('sw.js',{
+
+   scope:'./'
+
+	})
+	.then(reg=>{
+		console.log("success");
+		
+	})
+
+	.catch(error=>{
+		console.log("error");
+	});
+}
